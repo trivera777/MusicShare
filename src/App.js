9594579f957486f2251d8a2aba1ff488c8776807
Tifanny Rivera <trivera51580@gmail.com>
2022-01-02 21:43:38 -0700

    working on header component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
-import Header from './components/Header'
-import AddSong from './components/AddSong'
-import QueuedSong from './components/QueuedSong'
-import SongList from './components/SongList'
-import SongPlayer from './components/SongPlayer'
+import Header from "./components/Header";
+import AddSong from "./components/AddSong";
+import SongList from "./components/SongList";
+import SongPlayer from "./components/SongPlayer";
+import { Grid } from "@material-ui/core";
 
 function App() {
   return (
-  <div>
-    <Header />
-    <AddSong />
-    <QueuedSong />
-    <SongList />
-    <SongPlayer />
-    </div>
-  )
+    <>
+      <Header />
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7}>
+          <AddSong />
+          <SongList />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <SongPlayer />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import QueuedSongs from './QueuedSongs'
 
 function SongPlayer(){
-    return <div>player</div>
+    return (
+    <div>
+        player
+        <QueuedSongs />
+    </div>
+    )
 }
 
 export default SongPlayer;
