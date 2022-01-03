e27208544bb99821caae563708dd917d6ac394eb
Tifanny Rivera <trivera51580@gmail.com>
2022-01-02 18:19:30 -0700

    added necessary components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import Header from './components/Header'
+import AddSong from './components/AddSong'
+import QueuedSong from './components/QueuedSong'
+import SongList from './components/SongList'
+import SongPlayer from './components/SongPlayer'
 
 function App() {
   return (
   <div>
-    app
+    <Header />
+    <AddSong />
+    <QueuedSong />
+    <SongList />
+    <SongPlayer />
     </div>
   )
 }
