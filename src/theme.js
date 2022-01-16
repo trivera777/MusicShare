f3b49494afb7dcb3cdbcf05f72a0e752d13acdbb
Tifanny Rivera <trivera51580@gmail.com>
2022-01-15 19:36:08 -0700

    working on navbar and them colors

--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import {createTheme} from '@material-ui/core/styles'
-import { purple, teal } from '@material-ui/core/colors'
+import { pink, teal } from '@material-ui/core/colors'
 
 const theme = createTheme({
     palette: {
         type: "light",
-        primary: teal,
-        secondary: purple
+        primary: pink,
+        secondary: teal
     }
 });
 
