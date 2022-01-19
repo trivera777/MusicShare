bcb40fbd4e84de4b87c6cc40f71ad2adbb7d08f3
Tifanny Rivera <trivera51580@gmail.com>
2022-01-19 11:58:51 -0700

    added dialog to add song component

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,32 +1,38 @@
-import { InputAdornment, TextField, Button } from '@material-ui/core'
-import { Link, AddBoxOutlined } from "@material-ui/icons"
-import React from 'react'
+import { InputAdornment, TextField, Button, Dialog } from "@material-ui/core";
+import { Link, AddBoxOutlined } from "@material-ui/icons";
+import React from "react";
 
-function AddSong(){
-    return (
-        <div>
-            <TextField 
-            placeholder="Add Youtube or Soundcloud URL"
-            fullWidth
-            margin="normal"
-            type="url"
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment>
-                    {/* <Link /> */}
-                    </InputAdornment>
-                )
-            }}
-            />
-            <Button
-            variant="contained"
-            color="primary"
-            endIcon={<AddBoxOutlined />}
-            >
-                add
-            </Button>
-        </div>
-        )
+function AddSong() {
+    const [dialog, setDialog] = React.useState(false)
+
+    function handleSetDialog(){
+        setDialog(false)
+    }
+
+  return (
+    <div>
+        <Dialog
+        open={dialog}
+        onclose={handleSetDialog}
+        >
+            
+        </Dialog>
+      <TextField
+        placeholder="Add Youtube or Soundcloud URL"
+        fullWidth
+        margin="normal"
+        type="url"
+        InputProps={{
+          startAdornment: <InputAdornment>{/* <Link /> */}</InputAdornment>,
+        }}
+      />
+      <Button 
+      onClick={() => setDialog(true)}
+      variant="contained" color="primary" endIcon={<AddBoxOutlined />}>
+        add
+      </Button>
+    </div>
+  );
 }
 
 export default AddSong;
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,11 @@
 import {createTheme} from '@material-ui/core/styles'
-import { pink, teal } from '@material-ui/core/colors'
+import { pink } from '@material-ui/core/colors'
 
 const theme = createTheme({
     palette: {
         type: "dark",
         primary: pink,
-        secondary: teal
+        secondary: pink
     }
 });
 
