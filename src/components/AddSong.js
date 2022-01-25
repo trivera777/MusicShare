56bec13cc391f9524c6ae94e1e330fa97405c095
Tifanny Rivera <trivera51580@gmail.com>
2022-01-25 10:42:45 -0700

    editing dialog pop up window

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,34 +1,56 @@
-import { InputAdornment, TextField, Button, Dialog } from "@material-ui/core";
+import {
+  InputAdornment,
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+} from "@material-ui/core";
 import { Link, AddBoxOutlined } from "@material-ui/icons";
 import React from "react";
 
 function AddSong() {
-    const [dialog, setDialog] = React.useState(false)
+  const [dialog, setDialog] = React.useState(false);
 
-    function handleSetDialog(){
-        setDialog(false)
-    }
+  function handleSetDialog() {
+    setDialog(false);
+  }
 
   return (
     <div>
-        <Dialog
-        open={dialog}
-        onclose={handleSetDialog}
-        >
-            
-        </Dialog>
+      <Dialog open={dialog} onClose={handleSetDialog}>
+        <DialogTitle>Edit Song</DialogTitle>
+        <DialogContent>
+          <img src="" alt="Song thumbnail" />
+          <TextField margin="dense" name="title" label="Title" fullWidth />
+          <TextField margin="dense" name="artist" label="Artist" fullWidth />
+          <TextField
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+      </Dialog>
       <TextField
         placeholder="Add Youtube or Soundcloud URL"
         fullWidth
         margin="normal"
         type="url"
         InputProps={{
-          startAdornment: <InputAdornment>{/* <Link /> */}</InputAdornment>,
+          startAdornment: (
+            <InputAdornment>
+              <Link />
+            </InputAdornment>
+          ),
         }}
       />
-      <Button 
-      onClick={() => setDialog(true)}
-      variant="contained" color="primary" endIcon={<AddBoxOutlined />}>
+      <Button
+        onClick={() => setDialog(true)}
+        variant="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+      >
         add
       </Button>
     </div>
