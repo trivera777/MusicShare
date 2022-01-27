4ed99635cfdd522838588369c43f168f7038c260
Tifanny Rivera <trivera51580@gmail.com>
2022-01-26 19:36:12 -0700

    added play arrow and save icon to the song list component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,35 +1,73 @@
-import { CircularProgress } from '@material-ui/core';
-import React from 'react'
+import {
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  IconButton,
+  Typography,
+  Card,
+} from "@material-ui/core";
+import { PlayArrow, Save} from "@material-ui/icons"
+import React from "react";
 
-function SongList(){
-    let loading = false;
+function SongList() {
+  let loading = false;
 
-    const song = {
-        title: "after hours",
-        artist: "53 Thieves",
-        thumbnail: "https://www.youtube.com/watch?v=8cQSCIWHwRQ"
-    }
+  const song = {
+    title: "after hours",
+    artist: "53 Thieves",
+    thumbnail: "https://i.ytimg.com/an_webp/8cQSCIWHwRQ/mqdefault_6s.webp?du=3000&sqp=CKTTx48G&rs=AOn4CLBzUKfsQ8jTYzg5GGZR75yqHxEYZg",
+  };
 
-    if (loading){
-        return (
-            <div style={{
-                display:'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                marginTop: 50
-                
-            }}
-            >
-                <CircularProgress/>
-            </div>
-        )
-    }
-    return <div>{Array.from({ length: 10 }, () => song ).map((song, i) => (
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  }
+  return (
+    <div>
+      {Array.from({ length: 10 }, () => song).map((song, i) => (
         <Song key={i} song={song} />
-    ))}</div>
+      ))}
+    </div>
+  );
 }
 
-function Song(){
-    return <div>song</div>
+function Song({ song }) {
+  const { thumbnail, artist, title } = song;
+  return (
+    <Card>
+      <div>
+        <CardMedia image={thumbnail} />
+        <div>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {title}
+            </Typography>
+            <Typography variant="body1" component="p" color="textSecondary">
+              {title}
+            </Typography>
+          </CardContent>
+        <CardActions>
+          <IconButton size="small" color="primary">
+            <PlayArrow />
+          </IconButton>
+          <IconButton size="small" color="secondary">
+            <Save />
+          </IconButton>
+        </CardActions>
+        </div>
+      </div>
+    </Card>
+  );
 }
 export default SongList;
