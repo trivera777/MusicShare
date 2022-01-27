2f88b64d54f8bee2a74f70072cc4c937149b4216
Tifanny Rivera <trivera51580@gmail.com>
2022-01-27 12:57:59 -0700

    working song player component

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,10 +1,28 @@
+import { CardContent, Typography, Card, IconButton } from '@material-ui/core'
+import { SkipPrevious } from '@material-ui/icons'
 import React from 'react'
 import QueuedSongs from './QueuedSongs'
 
 function SongPlayer(){
     return (
     <div>
-        player
+        <Card varient="outlined">
+            <div>
+                <CardContent>
+                    <Typography variant="h5" component="h3">
+                        Title
+                    </Typography>
+                    <Typography variant="subtitle1" component="p" color="textSecondary">
+                        Artist
+                    </Typography>
+                </CardContent>
+                <div>
+                    <IconButton>
+                        <SkipPrevious />
+                    </IconButton>
+                    </div>
+            </div>
+        </Card>
         <QueuedSongs />
     </div>
     )
