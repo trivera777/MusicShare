c384c881f4e4c7ca044849e3dc6cdf9e1c5b8785
Tifanny Rivera <trivera51580@gmail.com>
2022-01-27 21:28:11 -0700

    song player now has thumbnail

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,31 +1,80 @@
-import { CardContent, Typography, Card, IconButton } from '@material-ui/core'
-import { SkipPrevious } from '@material-ui/icons'
-import React from 'react'
-import QueuedSongs from './QueuedSongs'
+import {
+  CardContent,
+  Typography,
+  Card,
+  IconButton,
+  Slider,
+  CardMedia,
+  makeStyles
+} from "@material-ui/core";
+import { PlayArrow, SkipPrevious, SkipNext } from "@material-ui/icons";
+import React from "react";
+import QueuedSongs from "./QueuedSongs";
 
-function SongPlayer(){
-    return (
-    <div>
-        <Card varient="outlined">
-            <div>
-                <CardContent>
-                    <Typography variant="h5" component="h3">
-                        Title
-                    </Typography>
-                    <Typography variant="subtitle1" component="p" color="textSecondary">
-                        Artist
-                    </Typography>
-                </CardContent>
-                <div>
-                    <IconButton>
-                        <SkipPrevious />
-                    </IconButton>
-                    </div>
-            </div>
-        </Card>
-        <QueuedSongs />
-    </div>
-    )
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '0px 15px'
+    },
+    content: {
+        flex: '1 0 auto'
+    },
+    thumbnail: {
+        width: 150
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingRight: theme.spacing(1)
+    },
+    playIcon: {
+        height: 38,
+        width: 38
+    }
+}))
+
+function SongPlayer() {
+    const classes = useStyles()
+
+  return (
+    <>
+      <Card variant="outlined" className={classes.container}>
+        <div className={classes.details}>
+          <CardContent className={classes.content}>
+            <Typography variant="h5" component="h3">
+              Title
+            </Typography>
+            <Typography variant="subtitle1" component="p" color="textSecondary">
+              Artist
+            </Typography>
+          </CardContent>
+          <div className={classes.controls}>
+            <IconButton>
+              <SkipPrevious />
+            </IconButton>
+            <IconButton>
+              <PlayArrow className={classes.playIcon} />
+            </IconButton>
+            <IconButton>
+              <SkipNext />
+            </IconButton>
+            <Typography variant="subtitle1" component="p" color="textSecondary">
+              00:01:30
+            </Typography>
+          </div>
+          <Slider type="range" min={0} max={1} step={0.01} />
+        </div>
+        <CardMedia className={classes.thumbnail} image="https://i.ytimg.com/an_webp/8cQSCIWHwRQ/mqdefault_6s.webp?du=3000&sqp=CKTTx48G&rs=AOn4CLBzUKfsQ8jTYzg5GGZR75yqHxEYZg" />
+      </Card>
+      <QueuedSongs />
+    </>
+  );
 }
 
 export default SongPlayer;
