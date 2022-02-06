0d285c802aa5c0a10dbb388248ac50498f31d66c
Tifanny Rivera <trivera51580@gmail.com>
2022-02-06 11:55:56 -0700

    added styling to queued song list

--- src/components/QueuedSongs.js
+++ src/components/QueuedSongs.js
@@ -1,53 +1,68 @@
-import { Avatar, IconButton, Typography, makeStyles } from '@material-ui/core'
+import { Avatar, IconButton, Typography, makeStyles } from "@material-ui/core";
 import { Delete } from "@material-ui/icons";
-import React from 'react'
+import React from "react";
 
-function QueuedSongList(){
-    const song = {
-        title: "after hours",
-        artist: "53 Thieves",
-        thumbnail:
-          "https://i.ytimg.com/an_webp/8cQSCIWHwRQ/mqdefault_6s.webp?du=3000&sqp=CKTTx48G&rs=AOn4CLBzUKfsQ8jTYzg5GGZR75yqHxEYZg",
-      };
+function QueuedSongList() {
+  const song = {
+    title: "after hours",
+    artist: "53 Thieves",
+    thumbnail:
+      "https://i.ytimg.com/an_webp/8cQSCIWHwRQ/mqdefault_6s.webp?du=3000&sqp=CKTTx48G&rs=AOn4CLBzUKfsQ8jTYzg5GGZR75yqHxEYZg",
+  };
 
-    return (
-        <div style={{ margin: '10px 0'}}>
-            <Typography color="textSecondary" variant="button">
-                Queue (5)
-            </Typography>
-            {Array.from({ length: 5 }, () => song).map((song, i) => (
-                <QueuedSong key={i} song={song} />
-            ))}
-        </div>
-    ) 
-}
-
-makeStyles({
-    avatar: {
-        width: 44,
-        height: 44,
-    },
-    te
-})
-
-function QueuedSong({ song }){
-    const { thumbnail, artist, title } = song
-    return (
-    <div>
-     <Avatar src={thumbnail} alt="Song Thumbnail" />
-     <div>
-         <Typography variant="subtitle2">
-             {title}
-         </Typography>
-         <Typography color="textSecondary" variant="body2">
-             {artist}
-         </Typography>
-     </div>
-     <IconButton>
-         <Delete color="error" />
-     </IconButton>
+  return (
+    <div style={{ margin: "10px 0" }}>
+      <Typography color="textSecondary" variant="button">
+        Queue (5)
+      </Typography>
+      {Array.from({ length: 5 }, () => song).map((song, i) => (
+        <QueuedSong key={i} song={song} />
+      ))}
     </div>
-        )
+  );
 }
+
+const useStyles = makeStyles({
+  avatar: {
+    width: 44,
+    height: 44,
+  },
+  text: {
+    textOverflow: "ellipsis",
+    overflow: "hidden",
+  },
+  container: {
+    display: "grid",
+    gridAutoFlow: "column",
+    gridTemplateColumns: "50px auto 50px",
+    gridGap: 12,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  songInfoContainer: {
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+  },
+});
+
+function QueuedSong({ song }) {
+  const classes = useStyles();
+  const { thumbnail, artist, title } = song;
+
+  return (
+    <div className={classes.container}>
+      <Avatar src={thumbnail} alt="Song Thumbnail" className={classes.avatar} />
+      <div className={classes.songInfoContainer} >
+        <Typography variant="subtitle2" className={classes.text}>{title}</Typography>
+        <Typography color="textSecondary" variant="body2" className={classes.text}>
+          {artist}
+        </Typography>
+      </div>
+      <IconButton>
+        <Delete color="error" />
+      </IconButton>
+    </div>
+  )
+};
 
 export default QueuedSongList;
